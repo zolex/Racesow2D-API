bae266081989e32d702214136d25bc411619370e
zolex <zlx@gmx.de>
2012-04-24 01:23:51 +0200

    removed old code and trailing whitespaces

--- downloads.php
+++ downloads.php
@@ -1,82 +1,64 @@
 <?php
 
 require('../dbh.php');
-if (isset($_GET['map'])) {
+$writer = new XMLWriter('1.0', 'utf-8');
+$writer->openURI('php://output');
+$writer->setIndent(false);
+$writer->startDocument();
 
-	$stmt = $dbh->prepare("SELECT `name`, `download` FROM `maps` WHERE `filename` = :filename;");
-	$stmt->bindValue("filename", $_GET['map']);
+try {
+
+	$stmt = $dbh->prepare("SELECT * FROM `maps` WHERE `download` IS NOT NULL ORDER BY name ASC;");
+
 	if (!$stmt->execute()) {
 	
-		die();
-	}
-	
-	if ($map = $stmt->fetchObject()) {
-	
-		echo $map->name . ',' . $map->download;
-		exit;
+		throw new Exception("Internal error. Please try again later.");
 	}
 
-} else {
+	$writer->startElement('maps');
 
-	$writer = new XMLWriter('1.0', 'utf-8');
-	$writer->openURI('php://output');
-	$writer->setIndent(false); 
-	$writer->startDocument();
+	while ($map = $stmt->fetchObject()) {
 
-	try {	
-		
-		$stmt = $dbh->prepare("SELECT * FROM `maps` WHERE `download` IS NOT NULL ORDER BY name ASC;");
-		
-		if (!$stmt->execute()) {
-		
-			throw new Exception("Internal error. Please try again later.");
-		}
-		
-		$writer->startElement('maps');
-		
-		while ($map = $stmt->fetchObject()) {
-		
-			$writer->startElement('map');
-			
-			$writer->startElement('id');
-			$writer->text($map->id);
-			$writer->endElement();	
-			
-			$writer->startElement('name');
-			$writer->text($map->name);
-			$writer->endElement();		
-			
-			$writer->startElement('author');
-			$writer->text($map->author);
-			$writer->endElement();			
-			
-			$writer->startElement('skill');
-			$writer->text($map->skill);
-			$writer->endElement();		
+		$writer->startElement('map');
 
-			$writer->startElement('download');
-			$writer->text($map->download);
-			$writer->endElement();
-
-			$writer->startElement('filename');
-			$writer->text($map->filename);
-			$writer->endElement();	
-			
-			$writer->endElement();		
-		}
-			
+		$writer->startElement('id');
+		$writer->text($map->id);
 		$writer->endElement();
-			
-	} catch (Exception $e) {
 
-		$writer->startElement('error');
-		$writer->startCData();
-		$writer->text($e->getMessage());
-		$writer->endCData();
+		$writer->startElement('name');
+		$writer->text($map->name);
+		$writer->endElement();
+
+		$writer->startElement('author');
+		$writer->text($map->author);
+		$writer->endElement();
+
+		$writer->startElement('skill');
+		$writer->text($map->skill);
+		$writer->endElement();
+
+		$writer->startElement('download');
+		$writer->text($map->download);
+		$writer->endElement();
+
+		$writer->startElement('filename');
+		$writer->text($map->filename);
+		$writer->endElement();
+
 		$writer->endElement();
 	}
 
-	header('Content-type: text/xml');
-	$writer->endDocument();
-	echo $writer->outputMemory();
+	$writer->endElement();
+
+} catch (Exception $e) {
+
+	$writer->startElement('error');
+	$writer->startCData();
+	$writer->text($e->getMessage());
+	$writer->endCData();
+	$writer->endElement();
 }
+
+header('Content-type: text/xml');
+$writer->endDocument();
+echo $writer->outputMemory();
